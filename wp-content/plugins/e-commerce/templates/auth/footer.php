eb79d24d185c96f1ec4c5b04d8ec4de2b6e0a382
feliphegomez <feliphegomez>
2021-12-14 18:54:55 -0500

    first commit

---
+++ wp-content/plugins/e-commerce/templates/auth/footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Auth footer
+ *
+ * This template can be overridden by copying it to yourtheme/classic-commerce/auth/footer.php.
+ *
+ * @see 	https://classiccommerce.cc/docs/installation-and-setup/template-structure/
+ * @author  WooThemes
+ * @package ClassicCommerce/Templates/Auth
+ * @version WC-2.4.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+		</div>
+	</body>
+</html>
---
+++ wp-content/plugins/e-commerce/templates/loop/loop-end.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Product Loop End
+ *
+ * This template can be overridden by copying it to yourtheme/classic-commerce/loop/loop-end.php.
+ *
+ * @see     https://classiccommerce.cc/docs/installation-and-setup/template-structure/
+ * @author  WooThemes
+ * @package ClassicCommerce/Templates
+ * @version WC-2.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+</ul>
