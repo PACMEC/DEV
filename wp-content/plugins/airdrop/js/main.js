c284f1bbf0c2db00309df96b8871edecc1ed5b1e
feliphegomez <feliphegomez>
2021-12-15 19:34:11 -0500

    first commit

--- wp-content/plugins/airdrop/js/main.js
+++
@@ -1,8 +0,0 @@
-(function( $ ) {
- 
-    // Add Color Picker to all inputs that have 'airdrops-manager-color-picker' class
-    $(function() {
-        $('.airdrops-manager-color-picker').wpColorPicker();
-    });
-     
-})( jQuery );
